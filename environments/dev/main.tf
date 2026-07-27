7974c1eaba2909cbed86f6262fc5cb6dae6706fe
Ujjwal <ujjals267@gmail.com>
2026-07-27 12:18:20 +0530

    ci: retest OIDC after trust policy fix v2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -57,3 +57,4 @@ output "vpc_id"       { value = module.vpc.vpc_id }
 output "bucket_id"    { value = module.app_bucket.bucket_id }
 output "web_ip"       { value = module.web_server.private_ip }
 output "sns_topic_arn" { value = aws_sns_topic.alerts.arn }# ci-debug-trigger
+# ci-debug-trigger-3
